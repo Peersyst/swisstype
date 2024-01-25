e3f37169fe173e2bbe080d537cc77e9c7fd59647
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2024-01-25 15:24:06 +0100

    Revert "fix(essential): Fix plain object types"
    This reverts commit 9705b37c9a6d945b71bfbacf67c700b3b2d46377.

--- packages/essential/index.d.ts
+++ packages/essential/index.d.ts
@@ -4,11 +4,6 @@
 export type AnyObject = Record<string | symbol | number, any>;
 
 /**
- * Plain object
- */
-export type PlainObject = Extract<object, any[] | null>;
-
-/**
  * Like `T & U`, but using the value types from `U` where their properties overlap.
  */
 export type Overwrite<T, U> = DistributiveOmit<T, keyof U> & U;
@@ -49,39 +44,39 @@ export type DeepPartialExcept<T, K extends keyof T> = DeepPartial<T> & Pick<T, K
 /**
  * Makes properties of type K required
  */
-export type Demand<T extends PlainObject, K extends keyof T> = T & { [P in K]-?: T[P] };
+export type Demand<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 
 /**
  * Makes properties of type K optional
  */
-export type Loosen<T extends PlainObject, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type Loosen<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /**
  * Makes properties of type K optional deeply
  */
-export type LoosenDeeply<T extends PlainObject, K extends keyof T> = Omit<T, K> & DeepPartial<Pick<T, K>>;
+export type LoosenDeeply<T, K extends keyof T> = Omit<T, K> & DeepPartial<Pick<T, K>>;
 
 /**
  * Get all keys with type T from O
  */
-export type TypeKeys<O extends PlainObject, T> = {
+export type TypeKeys<O, T> = {
     [K in keyof O]: O[K] extends T ? K : never;
 }[keyof O];
 
 /**
  * Removes properties of type T from O
  */
-export type OmitType<O extends PlainObject, T> = Omit<O, TypeKeys<O, T>>;
+export type OmitType<O, T> = Omit<O, TypeKeys<O, T>>;
 
 /**
  * Gets all keys with type undefined of T
  */
-export type UndefinedKeys<T extends PlainObject> = TypeKeys<T, undefined>;
+export type UndefinedKeys<T> = TypeKeys<T, undefined>;
 
 /**
  * Removes properties of type undefined from T
  */
-export type Defined<T extends PlainObject> = OmitType<T, undefined>;
+export type Defined<T> = OmitType<T, undefined>;
 
 /**
  * All possible iterations for a recursive type
@@ -93,28 +88,28 @@ type Iterations = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 /**
  * Get nested keys from T in the form of key1.key2...
  */
-export type NestedKeys<T extends PlainObject> = CoreNestedKeys<T>;
-type CoreNestedKeys<T extends PlainObject, I extends number = MaxRecursiveIterations> = I extends 0
+export type NestedKeys<T extends object> = CoreNestedKeys<T>;
+type CoreNestedKeys<T extends object, I extends number = MaxRecursiveIterations> = I extends 0
     ? never
     : {
-          [Key in keyof T]: T[Key] extends PlainObject ? `${Key}` | `${Key}.${CoreNestedKeys<T[Key], Iterations[I]>}` : Key;
+          [Key in keyof T]: T[Key] extends object ? `${Key}` | `${Key}.${CoreNestedKeys<T[Key], Iterations[I]>}` : Key;
       }[Extract<keyof T, string>];
 
 /**
  * Get nested keys from T in the form of key1.key2...
  */
-export type FlattenedNestedKeys<T extends PlainObject> = FlattenedCoreNestedKeys<T>;
-type FlattenedCoreNestedKeys<T extends PlainObject, I extends number = MaxRecursiveIterations> = I extends 0
+export type FlattenedNestedKeys<T extends object> = FlattenedCoreNestedKeys<T>;
+type FlattenedCoreNestedKeys<T extends object, I extends number = MaxRecursiveIterations> = I extends 0
     ? never
     : {
-          [Key in keyof T]: T[Key] extends PlainObject ? `${Key}.${CoreNestedKeys<T[Key], Iterations[I]>}` : Key;
+          [Key in keyof T]: T[Key] extends object ? `${Key}.${CoreNestedKeys<T[Key], Iterations[I]>}` : Key;
       }[Extract<keyof T, string>];
 
 /**
  * Pick K types from T with keys in the form of key1.key2...
  */
-export type DeepPick<T extends PlainObject, K extends NestedKeys<T>> = CoreDeepPick<T, K>;
-type CoreDeepPick<T extends PlainObject, K extends string, I extends number = MaxRecursiveIterations> = I extends 0
+export type DeepPick<T extends object, K extends NestedKeys<T>> = CoreDeepPick<T, K>;
+type CoreDeepPick<T extends object, K extends string, I extends number = MaxRecursiveIterations> = I extends 0
     ? never
     : K extends `${infer FirstKey}.${infer RestKey}`
       ? CoreDeepPick<T[FirstKey], RestKey, Iterations[I]>
@@ -123,19 +118,19 @@ type CoreDeepPick<T extends PlainObject, K extends string, I extends number = Ma
 /**
  * Overrides type T with O
  */
-export type Override<T extends PlainObject, O extends PlainObject> = {
+export type Override<T extends AnyObject, O extends AnyObject> = {
     [K in keyof T]: K extends keyof O ? O[K] : T[K];
 };
 
 /**
  * Deeply overrides type T with O
  */
-export type DeepOverride<T extends PlainObject, O extends PlainObject> = CoreDeepOverride<T, O>;
-type CoreDeepOverride<T extends PlainObject, O extends PlainObject, I extends number = MaxRecursiveIterations> = I extends 0
+export type DeepOverride<T extends AnyObject, O extends AnyObject> = CoreDeepOverride<T, O>;
+type CoreDeepOverride<T extends AnyObject, O extends AnyObject, I extends number = MaxRecursiveIterations> = I extends 0
     ? never
     : Override<
           {
-              [K in keyof T]: T[K] extends PlainObject | undefined ? DeepOverride<T[K], O> : T[K];
+              [K in keyof T]: T[K] extends AnyObject | undefined ? DeepOverride<T[K], O> : T[K];
           },
           O
       >;
@@ -143,17 +138,17 @@ type CoreDeepOverride<T extends PlainObject, O extends PlainObject, I extends nu
 /**
  * Injects type O into type T
  */
-export type Inject<T extends PlainObject, O extends PlainObject> = T & O;
+export type Inject<T extends AnyObject, O extends AnyObject> = T & O;
 
 /**
  * Deeply injects type O into type T
  */
-export type DeepInject<T extends PlainObject, O extends PlainObject> = CoreDeepInject<T, O>;
-type CoreDeepInject<T extends PlainObject, O extends PlainObject, I extends number = MaxRecursiveIterations> = I extends 0
+export type DeepInject<T extends AnyObject, O extends AnyObject> = CoreDeepInject<T, O>;
+type CoreDeepInject<T extends AnyObject, O extends AnyObject, I extends number = MaxRecursiveIterations> = I extends 0
     ? never
     : Inject<
           {
-              [K in keyof T]: T[K] extends PlainObject | undefined ? DeepInject<T[K], O> : T[K];
+              [K in keyof T]: T[K] extends AnyObject | undefined ? DeepInject<T[K], O> : T[K];
           },
           O
       >;
@@ -167,13 +162,13 @@ export type Join<U> = (U extends any ? (k: U) => void : never) extends (k: infer
 /**
  * Omit required properties from T
  */
-export type OmitRequired<T extends PlainObject> = {
+export type OmitRequired<T extends AnyObject> = {
     [K in keyof T as Pick<T, K> extends Pick<Required<T>, K> ? never : K]: T[K];
 };
 
 /**
  * Omit optional properties from T
  */
-export type OmitOptional<T extends PlainObject> = {
+export type OmitOptional<T> = {
     [K in keyof T as Pick<T, K> extends Pick<Required<T>, K> ? K : never]: T[K];
 };
